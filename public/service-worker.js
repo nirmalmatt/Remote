4bfda458689ecca3b7363edfb32e2c77e604622d
nirmalmatt <60476130+nirmalmatt@users.noreply.github.com>
2021-09-25 12:52:44 +0530

    fixed pwa error

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,6 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox
 
 console.log("Service Worker running")
 workbox.routing.registerRoute(
-    ({request}) => request.destination === 'script' || request.destination === "style",
-    new workbox.StaleWhileRevalidate()
+    ({request}) => request.destination === 'html',
+    new workbox.startegies.StaleWhileRevalidate()
   );
